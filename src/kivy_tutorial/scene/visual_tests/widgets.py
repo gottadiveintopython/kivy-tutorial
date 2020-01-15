8c4af4a32799da361781a97517ce3ee8cd5d83db
gottadiveintopython <flow4re2c@gmail.com>
2020-01-15 15:05:53 +0900

    add some basic widgets

---
+++ src/kivy_tutorial/scene/visual_tests/widgets.py
@@ -0,0 +1,56 @@
+# export KIVY_TUTORIAL_FIRST_SCENE=visual_tests.widgets
+
+KV_CODE = '''
+BoxLayout:
+    orientation: 'vertical'
+    spacing: 10
+    padding: 10
+    Label:
+        text: 'widgetの見た目のtest'
+        size_hint_y: None
+        height: self.texture_size[1]
+    BoxLayout:
+        spacing: 10
+        KTIconButton:
+            icon: 'arrow-right'
+        KTIconButton:
+            icon: 'twitter'
+        KTIconButton:
+            icon: 'twitch'
+        KTIconButton:
+            icon: 'blender-software'
+    BoxLayout:
+        spacing: 10
+        KTIcon:
+            icon: 'menu'
+        KTIcon:
+            icon: 'menu-open'
+        KTIcon:
+            icon: 'folder-clock'
+        KTIcon:
+            icon: 'menu-right'
+    KTButton:
+        text: "Beautiful is better than ugly."
+    KTButton:
+        text: "Explicit is better than implicit."
+    KTButton:
+        text: "Simple is better than complex."
+    KTButton:
+        text: "Complex is better than complicated."
+    KTButton:
+        text: "Flat is better than nested."
+'''
+
+
+async def main(switcher, nursery, *, parent, **kwargs):
+    import trio
+    from kivy.lang import Builder
+    import kivy_tutorial.widgets
+    from kivy_tutorial.triouser import activate_nursery
+    try:
+        with activate_nursery(nursery):
+            root = Builder.load_string(KV_CODE)
+        parent.add_widget(root)
+        await trio.sleep_forever()
+    finally:
+        parent.remove_widget(root)
---
+++ src/kivy_tutorial/widgets.py
@@ -0,0 +1,150 @@
+__all__ = ('KTIcon', 'KTButton', 'KTIconButton', )
+
+from kivy.properties import StringProperty, ColorProperty, NumericProperty
+from kivy.lang import Builder
+from kivy.factory import Factory
+
+import trio
+
+from kivy_tutorial.triouser import TrioUser
+
+
+KV_CODE = '''
+#:import md_icons kivymd.icon_definitions.md_icons
+
+<KTIcon>:
+    font_name: 'Icon'
+    text: md_icons.get(self.icon, '')
+
+<KTButton>:
+    outline_width: 2
+    outline_color: 1, 1, 1, 1
+    color: 0, 0, 0, 1
+    font_size: max(sp(15), 30)
+    canvas.before:
+        PushMatrix:
+        Scale:
+            origin: self.center
+            x: self._scaling
+            y: self._scaling
+        Color:
+            rgba: self._border_color
+        Rectangle:
+            pos: self.pos
+            size: self.size
+        Color:
+            rgba: self.background_color
+        Rectangle:
+            pos: self.x + dp(4), self.y + dp(4)
+            size: self.width - dp(8), self.height - dp(8)
+        PopMatrix:
+
+<KTIconButton>:
+    font_name: 'Icon'
+    text: md_icons.get(self.icon, '')
+    font_size: max(sp(20), 40)
+    size_hint: None, None
+    width: max(sp(30), 60)
+    height: max(sp(30), 60)
+    canvas.before:
+        Clear:
+        PushMatrix:
+        Scale:
+            origin: self.center
+            x: self._scaling
+            y: self._scaling
+        Color:
+            rgba: self._border_color
+        Ellipse:
+            pos: self.pos
+            size: self.size
+        Color:
+            rgba: self.background_color
+        Ellipse:
+            pos: self.x + dp(4), self.y + dp(4)
+            size: self.width - dp(8), self.height - dp(8)
+        PopMatrix:
+'''
+Builder.load_string(KV_CODE)
+
+
+class KTIcon(Factory.Label):
+    icon = StringProperty()
+
+
+class KTButton(TrioUser, Factory.Label):
+    __events__ = ('on_press', 'on_release', )
+    border_color = ColorProperty('#444444')
+    border_color2 = ColorProperty('#AAAAAA')
+    background_color = ColorProperty('#999933')
+    _border_color = ColorProperty(border_color.defaultvalue)
+    _scaling = NumericProperty(1)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.nursery.start_soon(self._trio_main)
+
+    def on_press(self):
+        pass
+
+    def on_release(self):
+        pass
+
+    async def _trio_main(self):
+        import trio
+        import asynckivy
+        try:
+            coro = self._asynckivy_main()
+            asynckivy.start(coro)
+            await trio.sleep_forever()
+        finally:
+            coro.close()
+
+    async def _asynckivy_main(self):
+        import asynckivy
+        from asynckivy import animation, or_, event
+        
+        coro_blink = None
+        current_touch = None
+        try:
+            while True:
+                __, current_touch = await event(
+                    self, 'on_touch_down',
+                    filter=lambda w, t: w.collide_point(*t.opos) and not t.is_mouse_scrolling,
+                    return_value=True,
+                )
+                self.dispatch('on_press')
+                current_touch.grab(self)
+                coro_blink = self._blink()
+                asynckivy.start(coro_blink)
+                await event(self, 'on_touch_up',
+                    filter=lambda w, t: t is current_touch and t.grab_current is w,
+                    return_value=True,
+                )
+                coro_blink.close()
+                current_touch.ungrab(self)
+                if self.collide_point(*current_touch.pos):
+                    self.dispatch('on_release')
+                    await animation(self, _scaling=.8, d=.05)
+                    await animation(self, _scaling=1, d=.05)
+        finally:
+            if coro_blink is not None:
+                coro_blink.close()
+            if current_touch is not None:
+                current_touch.ungrab(self)
+
+
+    async def _blink(self):
+        from asynckivy import sleep
+        try:
+            while True:
+                await sleep(.1)
+                self._border_color = self.border_color2
+                await sleep(.1)
+                self._border_color = self.border_color
+        finally:
+            self._border_color = self.border_color
+
+
+class KTIconButton(KTButton):
+    icon = StringProperty()
