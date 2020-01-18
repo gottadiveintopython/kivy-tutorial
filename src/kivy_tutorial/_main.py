4516be5db874a8b9be3d903559fe8dcf7ba6c262
gottadiveintopython <flow4re2c@gmail.com>
2020-01-18 12:38:27 +0900

    add 'title' scene

--- src/kivy_tutorial/_main.py
+++ src/kivy_tutorial/_main.py
@@ -18,15 +18,19 @@ FloatLayout:
 async def main(*, nursery, parent):
     import os
     from kivy.lang import Builder
+    from kivy_tutorial.triouser import activate_nursery
     from kivy_tutorial.sceneswitcher import SceneSwitcher
+    from kivy_tutorial.bgmplayer import BgmPlayer
 
-    root = Builder.load_string(KV_CODE)
-    parent.add_widget(root)
-    switcher = SceneSwitcher(
-        nursery=nursery,
-        pkgname='kivy_tutorial.scene',
-        userdata={
-            'parent': root.ids.middle_layer,
-        },
-    )
+    with activate_nursery(nursery):
+        root = Builder.load_string(KV_CODE)
+        parent.add_widget(root)
+        switcher = SceneSwitcher(
+            pkgname='kivy_tutorial.scene',
+            userdata={
+                'parent': root.ids.middle_layer,
+                'bgmplayer': BgmPlayer(),
+            },
+        )
+    # switcher.switch('visual_tests.widgets')
     switcher.switch(os.environ.get('KIVY_TUTORIAL_FIRST_SCENE', 'title'))
---
+++ src/kivy_tutorial/scene/title.py
@@ -0,0 +1,64 @@
+async def main(switcher, nursery, *, parent, bgmplayer, **kwargs):
+    import trio
+    from functools import partial
+    from kivy.factory import Factory
+    from kivy_tutorial.asynchelper import or_, animation, event
+    from kivy_tutorial.widgets import KTTightLabel, KTTightButton
+
+    try:
+        bgmplayer.play('n75.ogg')
+        root = Factory.RelativeLayout()
+        parent.add_widget(root)
+
+        async def _show_title_label():
+            title_label = KTTightLabel(
+                text='Kivyへのいざない',
+                opacity=0,
+                font_size='70sp',
+                pos_hint={'center_x': .5, },
+            )
+            root.add_widget(title_label)
+            await trio.sleep(0.01)
+            title_label.y = root.height
+            await or_(
+                animation(
+                    title_label,
+                    d=3,
+                    opacity=1,
+                    center_y=root.height * 0.6,
+                    force_final_value=True,
+                ),
+                event(root, 'on_touch_down'),
+            )
+            title_label.pos_hint['center_y'] = 0.6
+            title_label.property('pos_hint').dispatch(title_label)
+        await _show_title_label()
+
+        start_button = None
+        async def _show_start_button():
+            nonlocal start_button
+            start_button = KTTightButton(
+                nursery=nursery,
+                text='開始',
+                opacity=0,
+                font_size='70sp',
+                pos_hint={'center_x': .5, 'center_y': .25, },
+            )
+            root.add_widget(start_button)
+            await trio.sleep(0.01)
+            await or_(
+                animation(
+                    start_button,
+                    d=3,
+                    opacity=1,
+                    force_final_value=True,
+                ),
+                event(root, 'on_touch_down'),
+            )
+        await _show_start_button()
+
+        await event(start_button, 'on_release')
+        await animation(root, opacity=0)
+        switcher.switch('menu')
+    finally:
+        parent.remove_widget(root)
