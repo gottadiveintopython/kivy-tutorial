0859ad2fd495a43c07d945bfd68ee187bf304428
gottadiveintopython <flow4re2c@gmail.com>
2020-01-22 19:31:20 +0900

    bgmplayerのtestをより精密に

--- src/tests/test_bgmplayer.py
+++ src/tests/test_bgmplayer.py
@@ -30,7 +30,7 @@ async def test_bgm_resumability(nursery):
     assert prev_pos < curr_pos
 
     
-async def test_play_then_stop(nursery):
+async def test_play_then_stop_then_play_the_same_one(nursery):
     import trio
     from kivy_tutorial.bgmplayer import BgmPlayer
     
@@ -40,21 +40,42 @@ async def test_play_then_stop(nursery):
         file_prefix='sound/'
     )
     bgms = player._bgms
+
     player.play('n75.ogg')
     assert player._next_file == 'n75.ogg'
-    await trio.sleep(.2)
+    assert 'n75.ogg' not in bgms
+    assert player._current_bgm is None
+
+    await trio.sleep(1.5)
     assert player._next_file is None
     assert 'n75.ogg' in bgms
     assert player._current_bgm is bgms['n75.ogg']
+
     player.stop()
     assert player._next_file == ''
-    await trio.sleep(3)
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n75.ogg']
+
+    await trio.sleep(1.5)
     assert player._next_file is None
     assert 'n75.ogg' in bgms
     assert player._current_bgm is None
 
+    player.play('n75.ogg')
+    assert player._next_file == 'n75.ogg'
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is None
 
-async def test_play_then_play(nursery):
+    await trio.sleep(1.5)
+    assert player._next_file is None
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n75.ogg']
+
+    player.stop()
+    await trio.sleep(1.5)
+
+
+async def test_play_then_play_the_same_one(nursery):
     import trio
     from kivy_tutorial.bgmplayer import BgmPlayer
     
@@ -64,16 +85,108 @@ async def test_play_then_play(nursery):
         file_prefix='sound/'
     )
     bgms = player._bgms
+
     player.play('n75.ogg')
     assert player._next_file == 'n75.ogg'
-    await trio.sleep(.2)
+    assert 'n75.ogg' not in bgms
+    assert player._current_bgm is None
+
+    await trio.sleep(1.5)
     assert player._next_file is None
     assert 'n75.ogg' in bgms
     assert player._current_bgm is bgms['n75.ogg']
+
+    player.play('n75.ogg')
+    assert player._next_file == 'n75.ogg'
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n75.ogg']
+
+    await trio.sleep(1.5)
+    assert player._next_file is None
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n75.ogg']
+
+    player.stop()
+    await trio.sleep(1.5)
+
+
+async def test_play_then_stop_then_play_another_one(nursery):
+    import trio
+    from kivy_tutorial.bgmplayer import BgmPlayer
+    
+    player = BgmPlayer(
+        nursery=nursery,
+        polling_interval=.1,
+        file_prefix='sound/'
+    )
+    bgms = player._bgms
+
+    player.play('n75.ogg')
+    assert player._next_file == 'n75.ogg'
+    assert 'n75.ogg' not in bgms
+    assert player._current_bgm is None
+
+    await trio.sleep(1.5)
+    assert player._next_file is None
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n75.ogg']
+
+    player.stop()
+    assert player._next_file == ''
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n75.ogg']
+
+    await trio.sleep(1.5)
+    assert player._next_file is None
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is None
+
     player.play('n74.ogg')
     assert player._next_file == 'n74.ogg'
-    await trio.sleep(3)
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is None
+
+    await trio.sleep(1.5)
     assert player._next_file is None
     assert 'n74.ogg' in bgms
     assert 'n75.ogg' in bgms
     assert player._current_bgm is bgms['n74.ogg']
+
+    player.stop()
+    await trio.sleep(1.5)
+
+
+async def test_play_then_play_another_one(nursery):
+    import trio
+    from kivy_tutorial.bgmplayer import BgmPlayer
+    
+    player = BgmPlayer(
+        nursery=nursery,
+        polling_interval=.1,
+        file_prefix='sound/'
+    )
+    bgms = player._bgms
+
+    player.play('n75.ogg')
+    assert player._next_file == 'n75.ogg'
+    assert 'n75.ogg' not in bgms
+    assert player._current_bgm is None
+
+    await trio.sleep(1.5)
+    assert player._next_file is None
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n75.ogg']
+
+    player.play('n74.ogg')
+    assert player._next_file == 'n74.ogg'
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n75.ogg']
+
+    await trio.sleep(2.5)
+    assert player._next_file is None
+    assert 'n74.ogg' in bgms
+    assert 'n75.ogg' in bgms
+    assert player._current_bgm is bgms['n74.ogg']
+
+    player.stop()
+    await trio.sleep(1.5)
