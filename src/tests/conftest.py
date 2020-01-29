2db64f049238dc4fc6884ca60dd946ddea65d06e
gottadiveintopython <flow4re2c@gmail.com>
2020-01-29 13:59:09 +0900

    configure for test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
-
+from kivy.tests.fixtures import kivy_app
 
 @pytest.fixture(scope='session')
 def assets_dir():
     from pathlib import Path
     return Path(__file__).parents[1] / 'assets'
+
